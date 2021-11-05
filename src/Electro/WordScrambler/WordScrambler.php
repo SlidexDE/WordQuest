c71bb471351c123a7d21504fe2f8fdeaca417e52
Electro Games <34932094+ElectroGamesYT@users.noreply.github.com>
2021-11-05 10:56:20 -0500

    Update WordScrambler.php

--- src/Electro/WordScrambler/WordScrambler.php
+++ src/Electro/WordScrambler/WordScrambler.php
@@ -1,1 +1,38 @@
 
+<?php
+
+namespace Electro\WordScrambler;
+
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+// use Config
+// use PlayerChatEvent
+use pocketmine\event\Listener;
+
+
+class ChatScrambler extends PluginBase implements Listener{
+
+    public $word = null;
+    public $words = [];
+
+    public function onChat(playerChatEvent $event)
+    {
+        $msg = $event->getMessage();
+
+        if ($msg == $this->word)
+        {
+            $this->word = null;
+        }
+    }
+
+
+    public function loadWords()
+    {
+        foreach($this->getConfig()->get("Words") as $word)
+        {
+            array_push($this->words, $word);
+        }
+    }
+
+}
