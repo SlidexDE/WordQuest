99067562e0d76e7a5a0a35865dd603193808da7b
Electro Games <2cool4u567@gmail.com>
2021-12-04 00:48:32 -0600

    Update WordScrambler.php

--- src/Electro/WordScrambler/WordScrambler.php
+++ src/Electro/WordScrambler/WordScrambler.php
@@ -1,26 +1,22 @@
-
 <?php
 
 namespace Electro\WordScrambler;
 
-use pocketmine\Player;
-
 use pocketmine\plugin\PluginBase;
-// use Config
-// use PlayerChatEvent
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\Listener;
+use onebone\economyapi\EconomyAPI;
 
+class WordScrambler extends PluginBase implements Listener{
 
-class ChatScrambler extends PluginBase implements Listener{
-
-    public $word = null;
-    public $words = [];
-
-    public function onEnable()
+    public ?string $word = null;
+    public float $reward;
+    public array $words = [];
+    public function onEnable() : void
     {
         $this->loadWords();
-        // Enable Listener
-        // Start Task
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getScheduler()->scheduleDelayedTask(new ScrambleTask($this), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
     }
 
     public function onChat(playerChatEvent $event)
@@ -28,8 +24,9 @@ class ChatScrambler extends PluginBase implements Listener{
         $player = $event->getPlayer();
         $msg = $event->getMessage();
 
-        if ($msg == $this->word)
+        if (strtolower($msg) == strtolower($this->word))
         {
+            $event->cancel();
             $this->word = null;
             $this->playerWon($player);
         }
@@ -40,13 +37,24 @@ class ChatScrambler extends PluginBase implements Listener{
     {
         foreach($this->getConfig()->get("Words") as $word)
         {
-            array_push($this->words, $word);
+            $this->words[] = $word;
         }
     }
     public function playerWon($player)
     {
-        $this->getServer()->broadcastMessage("§6" . $player->getName() . " Guessed The Word Correctly.\nThe Word Was " . $this->word);
-        // Give money
+        $this->getServer()->broadcastMessage("§6" . $player->getName() . " Guessed The Word Correctly.\n§6The Word Was §e" . $this->word);
+        EconomyAPI::getInstance()->addMoney($player, $this->reward);
+    }
+
+    public function scrambleWord()
+    {
+        $this->word = $this->words[array_rand($this->words)];
+        $this->reward = mt_rand($this->getConfig()->get("Min-Reward"), $this->getConfig()->get("Max-Reward"));
+        foreach($this->getServer()->getOnlinePlayers() as $player)
+        {
+            $player->sendMessage("§bFirst Player To Unscramble The Word §e". str_shuffle($this->word) ." §bWill Receive $". $this->reward ."!");
+        }
+        $this->getScheduler()->scheduleDelayedTask(new ScrambleTask($this), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
     }
 
 }
