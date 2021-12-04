2dcda4e3dbf4a44462e8fe12cf4b33e8a2fa7b58
Electro Games <2cool4u567@gmail.com>
2021-12-04 01:05:24 -0600

    Update WordScrambler.php

--- src/Electro/WordScrambler/WordScrambler.php
+++ src/Electro/WordScrambler/WordScrambler.php
@@ -11,9 +11,19 @@ class WordScrambler extends PluginBase implements Listener{
 
     public ?string $word = null;
     public float $reward;
+    public bool $rewardEnabled = false;
     public array $words = [];
     public function onEnable() : void
     {
+        if ($this->getConfig()->get("Reward-Enabled"))
+        {
+            $this->rewardEnabled = true;
+        }
+        if (!$this->getServer()->getPluginManager()->getPlugin("EconomyAPI") && $this->rewardEnabled == true)
+        {
+            $this->getLogger()->warning("Reward has been disabled since you do not have EconomyAPI installed on your server.");
+            $this->rewardEnabled = false;
+        }
         $this->loadWords();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getScheduler()->scheduleDelayedTask(new ScrambleTask($this), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
@@ -43,18 +53,30 @@ class WordScrambler extends PluginBase implements Listener{
     public function playerWon($player)
     {
         $this->getServer()->broadcastMessage("§6" . $player->getName() . " Guessed The Word Correctly.\n§6The Word Was §e" . $this->word);
-        EconomyAPI::getInstance()->addMoney($player, $this->reward);
+        if ($this->rewardEnabled)
+        {
+            EconomyAPI::getInstance()->addMoney($player, $this->reward);
+        }
     }
 
     public function scrambleWord()
     {
         $this->word = $this->words[array_rand($this->words)];
-        $this->reward = mt_rand($this->getConfig()->get("Min-Reward"), $this->getConfig()->get("Max-Reward"));
+        if ($this->rewardEnabled)
+        {
+            $this->reward = mt_rand($this->getConfig()->get("Min-Reward"), $this->getConfig()->get("Max-Reward"));
+        }
         foreach($this->getServer()->getOnlinePlayers() as $player)
         {
-            $player->sendMessage("§bFirst Player To Unscramble The Word §e". str_shuffle($this->word) ." §bWill Receive $". $this->reward ."!");
+            if ($this->rewardEnabled)
+            {
+                $player->sendMessage("§bFirst Player To Unscramble The Word §e". str_shuffle($this->word) ." §bWill Receive $". $this->reward ."!");
+            }
+            else
+            {
+                $player->sendMessage("§bTry to be the first player to unscramble §e". str_shuffle($this->word) . "!");
+            }
         }
         $this->getScheduler()->scheduleDelayedTask(new ScrambleTask($this), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
     }
-
 }
