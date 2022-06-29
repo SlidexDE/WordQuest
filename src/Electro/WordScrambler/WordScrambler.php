010e30b8e6a17c52037ca0003f6c623294d05fbb
SlidexDE <101284125+SlidexDE@users.noreply.github.com>
2022-06-29 20:04:21 +0200

    Update WordScrambler.php

--- src/Electro/WordScrambler/WordScrambler.php
+++ src/Electro/WordScrambler/WordScrambler.php
@@ -5,44 +5,51 @@ namespace Electro\WordScrambler;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\Listener;
-use onebone\economyapi\EconomyAPI;
+use cooldogedev\BedrockEconomy\api\version\LegacyBEAPI;
+use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
+use pocketmine\player\Player;
 
 class WordScrambler extends PluginBase implements Listener{
-
+    
     public ?string $word = null;
-    public float $reward;
+    public int $reward;
     public bool $rewardEnabled = false;
     public array $words = [];
+    
+    /** @var LegacyBEAPI*/
+    public $moneyAPI;
+    
     public function onEnable() : void
     {
         if ($this->getConfig()->get("Reward-Enabled"))
         {
             $this->rewardEnabled = true;
         }
-        if (!$this->getServer()->getPluginManager()->getPlugin("EconomyAPI") && $this->rewardEnabled == true)
+        if (!$this->getServer()->getPluginManager()->getPlugin("BedrockEconomy") && $this->rewardEnabled == true)
         {
-            $this->getLogger()->warning("Reward has been disabled since you do not have EconomyAPI installed on your server.");
+            $this->getLogger()->warning("Reward has been disabled since you do not have BedrockEconomy installed on your server.");
             $this->rewardEnabled = false;
         }
         $this->loadWords();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getScheduler()->scheduleDelayedTask(new ScrambleTask($this), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
+        $this->moneyAPI = BedrockEconomyAPI::legacy();
     }
-
+    
     public function onChat(playerChatEvent $event)
     {
         $player = $event->getPlayer();
         $msg = $event->getMessage();
-
+        
         if (strtolower($msg) == strtolower($this->word))
         {
-            $event->cancel();
-            $this->playerWon($player);
-            $this->word = null;
+           $event->cancel();
+           $this->playerWon($player);
+           $this->word = null;
         }
     }
-
-
+    
+    
     public function loadWords()
     {
         foreach($this->getConfig()->get("Words") as $word)
@@ -50,15 +57,15 @@ class WordScrambler extends PluginBase implements Listener{
             $this->words[] = $word;
         }
     }
-    public function playerWon($player)
+    public function playerWon(Player $player)
     {
         $this->getServer()->broadcastMessage("§6" . $player->getName() . " Guessed The Word Correctly.\n§6The Word Was §e" . $this->word);
         if ($this->rewardEnabled)
         {
-            EconomyAPI::getInstance()->addMoney($player, $this->reward);
+            $this->moneyAPI->addToPlayerBalance($player->getName(), $this->reward);
         }
     }
-
+    
     public function scrambleWord()
     {
         $this->word = $this->words[array_rand($this->words)];
